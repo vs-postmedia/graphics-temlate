9292144d516be561d76b69f15ec2fdd13895b137
Nathan Griffiths <ngriffiths@postmedia.com>
2020-03-26 16:44:54 -0700

    style updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-// imports
-import css from './css/main.css';
+// CSS
 import normalize from './css/normalize.css';
+import colours from './css/colors.css';
+import fonts from './css/fonts.css';
+import css from './css/main.css';
+
+// JS
 import test from './js/test.js'
 
 console.log('hello, world');
